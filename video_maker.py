17fe25ab47fecf4026eb3e0ec89df8249aa37468
Michael Rára <michaelrara24@gmail.com>
2022-07-09 14:08:32 +0200

    Add files via upload

---
+++ video_maker.py
@@ -0,0 +1,16 @@
+import cv2
+import os.path
+
+
+def build_video(image_folder, name_of_image):
+    index = 0
+    frame = cv2.imread(os.path.join(image_folder, name_of_image + str(index) + '.bmp'))
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter("A_star_Map_Points.avi", 0, 40, (width,height))
+    while os.path.isfile(image_folder + name_of_image + str(index) + '.bmp'):    
+        video.write(cv2.imread(os.path.join(image_folder, name_of_image + str(index) + '.bmp')))
+        index += 1
+    cv2.destroyAllWindows()
+    video.release()
+
+build_video('C:/Navmatix/git/d-lite/output/', 'Mapa_iterace_')
