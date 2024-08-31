d66ba1ae3e4f1355040da980498e3584060b88bc
Michael Rára <michaelrara24@gmail.com>
2024-08-31 12:43:44 +0200

    Update video_maker.py

--- video_maker.py
+++ video_maker.py
@@ -29,14 +29,15 @@ def build_video(image_folder: str,
     frame = cv2.imread(os.path.join(image_folder, prefix_of_image_name + str(index) + '.' + image_format))
     height, width, _ = frame.shape
     video = cv2.VideoWriter(name_of_output_video + ".avi", 0, frames_per_second, (width, height))
-    while os.path.isfile(image_folder + "/" + prefix_of_image_name + str(index) + '.' + image_format):
+    while os.path.isfile(os.path.join(image_folder, prefix_of_image_name + str(index) + '.' + image_format)):
         video.write(cv2.imread(os.path.join(image_folder, prefix_of_image_name + str(index) + '.' + image_format)))
         index += 1
     cv2.destroyAllWindows()
     video.release()
 
 
-def calculate_grayscale_gradient_in_pixel(image: Image, width_position: int, height_position: int) -> np.ndarray[float, float]:
+def calculate_grayscale_gradient_in_pixel(image: Image,
+                                          width_position: int, height_position: int) -> np.ndarray[float, float]:
     """Calculate gradient of provided image in pixel specified by width_position, height_position.
 
     Args:
@@ -78,15 +79,20 @@ def calculate_grayscale_gradient_in_pixel(image: Image, width_position: int, hei
     return np.array([horizontal_derivation, vertical_derivation])
 
 
-def calculate_gradient_of_grayscale_image(image: Image) -> Image:
-    """Calculate gradient image of provided image. Edges are visualized in this way. 
+def calculate_gradient_of_grayscale_image(image: Image, threshold_of_significance: float = 1) -> Image:
+    """Calculate gradient image of provided image. Edges are visualized in this way.
 
     Args:
         image (Image): Grayscale image to detect edges in.
+        threshold_of_significance (float): Defines what percentage of maximum value of gradient in image will be set to
+                                max brightness value 255. Must be higher than 0 and lower or equal to one. Default to 1.
 
     Returns:
         Image: Grayscale image where edges are visualized.
     """
+    if threshold_of_significance > 1 or threshold_of_significance <= 0:
+        raise ValueError("Parameter threshold_of_significance must be higher than 0 and smaller or equal to 1.")
+
     image = np.asarray(image)
 
     # Horizontal derivation
@@ -104,7 +110,14 @@ def calculate_gradient_of_grayscale_image(image: Image) -> Image:
     vertical_derivation = frame_up - frame_down
 
     # Calculate norm of gradient
-    return img.fromarray(np.sqrt(horizontal_derivation**2 + vertical_derivation**2))
+    grad_image = np.sqrt(horizontal_derivation**2 + vertical_derivation**2)
+
+    # Recalculate values of brightness with a help of threshold_of_significance
+    max_gradient = np.max(grad_image)
+    max_brightness = threshold_of_significance * max_gradient
+    recalculated_brightness = 255/max_brightness * grad_image
+    edges = np.clip(recalculated_brightness, 0, 255)
+    return img.fromarray(edges).convert("L")
 
 
 def convert_rgb_image_to_grayscale_image(image: Image) -> Image:
@@ -134,25 +147,33 @@ def create_rgb_image(width: int, height: int) -> Image:
     return img.new("RGB", (width, height), "white")
 
 
-def denoise_image(frames_to_remove_noise: List[Image]) -> None:
-    """Accepts list of original grayscale frames and remove noise out of them. Output frames are saved into current
-    working directory.
+def denoise_video(path_to_video: str, directory_for_results: str) -> None:
+    """Iterates through every frame of input video and suppress noise in it.
+    First two frames are not returned and last two frames are not returned.
 
     This method is used for noise removing.
-    https://docs.opencv.org/4.x/d1/d79/group__photo__denoise.html#ga723ffde1969430fede9241402e198151
+    #https://docs.opencv.org/4.x/d1/d79/group__photo__denoise.html#ga723ffde1969430fede9241402e198151
 
     Args:
-        frames_to_remove_noise (List[Image]): List of frames to remove noise from.
+        path_to_video (str): Path to video to remove noise from.
+        directory_for_results (str): Name of folder where frames with suppress noise are stored.
     """
-    for i in range(2, len(frames_to_remove_noise) - 1):
-        denoised_frame = cv2.fastNlMeansDenoisingMulti(srcImgs=frames_to_remove_noise,
+    cap = cv2.VideoCapture(path_to_video)
+    success, frame = cap.read()
+    images_of_input_video = []
+    while success:  # Capture frame-by-frame
+        images_of_input_video.append(frame)
+        success, frame = cap.read()
+    os.mkdir(directory_for_results)
+    for i in range(2, len(images_of_input_video) - 2):
+        denoised_frame = cv2.fastNlMeansDenoisingMulti(srcImgs=images_of_input_video,
                                                        imgToDenoiseIndex=i,
                                                        temporalWindowSize=5,
                                                        dst=None,
-                                                       templateWindowSize=4,
+                                                       templateWindowSize=5,
                                                        searchWindowSize=7,
                                                        h=35)
-        cv2.imwrite("DenoisedFrame_" + str(i) + ".png", denoised_frame)
+        cv2.imwrite(directory_for_results+"/denoised_frame_" + str(i) + ".png", denoised_frame)
 
 
 def get_brightness_of_pixel(image: Image, width_position: int, height_position: int) -> int:
@@ -215,6 +236,31 @@ def make_grayscale_image_sharper(image: Image) -> Image:
     return img.fromarray(output_sharpen_image.astype("uint8"))
 
 
+def make_grayscale_image_sharper_in_selected_pixels(image: Image,
+                                                    pixels_to_sharper: List) -> Image:
+    """Sharper input image in selected pixels if it is possible.
+
+    Args:
+        image (Image): Input image to sharper in selected pixels.
+        pixels_to_sharper (List): List of pixels where to make input image sharper.
+
+    Returns:
+        Image: Input image which was sharpened in selected pixels.
+    """
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5, -1],
+                       [0, -1, 0]])
+    input_image_pixels = np.asarray(image)
+    sharpen_image_pixels = np.array(image).astype("float")
+    for j, i in pixels_to_sharper:
+        if j < 1 or j > image.height - 2 or i < 1 or i > image.width - 2:
+            continue
+        window_to_sharp = input_image_pixels[j-1:j+2, i-1:i+2]
+        sharpen_image_pixels[j, i] = np.sum(window_to_sharp*kernel).item()
+    output_sharpen_image = np.clip(sharpen_image_pixels, 0, 255)
+    return img.fromarray(output_sharpen_image.astype("uint8"))
+
+
 def put_images_side_by_side(image_folder: str,
                             name_of_left_frame: str,
                             name_of_right_frame: str,
@@ -234,18 +280,39 @@ def put_images_side_by_side(image_folder: str,
     """
     os.mkdir(directory_for_results)
     while os.path.isfile(image_folder + "/" + name_of_left_frame + str(index) + "." + img_format):
-        original_frame = cv2.imread(os.path.join(image_folder, name_of_left_frame
-                                                 + str(index)
-                                                 + "."
-                                                 + img_format))
-        upgraded_frame = cv2.imread(os.path.join(image_folder, name_of_right_frame
-                                                 + str(index)
-                                                 + "."
-                                                 + img_format))
+        original_frame = cv2.imread(os.path.join(image_folder, name_of_left_frame + str(index) + "." + img_format))
+        upgraded_frame = cv2.imread(os.path.join(image_folder, name_of_right_frame + str(index) + "." + img_format))
         concatenated_frame = np.concatenate((original_frame, upgraded_frame), axis=1)
         cv2.imwrite(directory_for_results + "/" + 'parallel_frames_' + str(index) + "." + img_format,
                     concatenated_frame)
         index += 1
+
+
+def sharper_frames_along_most_significant_edges(directory_for_results: str,
+                                                folder_of_input_frames: str,
+                                                image_prefix: str,
+                                                threshold_of_significance: float,
+                                                min_value_of_significant_edge: int):
+    """Iterates over frames. Pixels of significant edges are detected in every frame. Image is sharpened in positions of
+    these pixels.
+
+    Args:
+        directory_for_results (str): Folder where output of this method are stored.
+        folder_of_input_frames (str): Folder where inputs are uploaded.
+        image_prefix (str): Prefix name of image to process.
+        threshold_of_significance (float): Defines the percentage of value of max gradient. Pixels with this value will
+            have maximum value of brightness 255.
+        min_value_of_significant_edge (int): Edges with brightness with value higher than is specified by this parameter
+            will be considered significant.
+    """
+    os.mkdir(directory_for_results)
+    for i in range(2, 12):
+        original_frame = img.open(folder_of_input_frames + "/" + image_prefix + str(i) + ".png")
+        original_frame_grayscale = convert_rgb_image_to_grayscale_image(original_frame)
+        frame_of_edges = calculate_gradient_of_grayscale_image(original_frame_grayscale, threshold_of_significance)
+        significant_edges = np.argwhere(np.asarray(frame_of_edges) > min_value_of_significant_edge).tolist()
+        enhanced_image = make_grayscale_image_sharper_in_selected_pixels(original_frame_grayscale, significant_edges)
+        enhanced_image.save(directory_for_results + "/" + "enhanced_image_" + str(i) + ".png")
 
 
 def split_video_to_frames(directory_for_results: str, frame_output_format: str, path_to_video: str) -> None:
@@ -267,15 +334,34 @@ def split_video_to_frames(directory_for_results: str, frame_output_format: str, 
 
 
 def main() -> None:
+    """
     build_video(image_folder='Chessboard_images',
                 prefix_of_image_name='Chessboard_',
                 image_format="png",
                 frames_per_second=1,
                 name_of_output_video="chessboard_moves")
-
     split_video_to_frames("chess_video",
                           frame_output_format="png",
                           path_to_video='C:/Users/A200179575/Python_Projects/personal/video_maker/chessboard_moves.avi')
+    """
+    dir_for_denoised_frames = "denoised_frames"
+    """
+    denoise_video(path_to_video="chessboard_moves.avi", directory_for_results=dir_for_denoised_frames)
+
+    directory_of_frame_edges = "frames_of_edges"
+    os.mkdir(directory_of_frame_edges)
+    for img_index in range(2, 12):
+        grayscale_image = convert_rgb_image_to_grayscale_image(
+                                                img.open("denoised_frames/denoised_frame_" + str(img_index) + ".png"))
+
+        frame_of_edges = calculate_gradient_of_grayscale_image(grayscale_image)
+        frame_of_edges.save(directory_of_frame_edges + "/edges_" + str(img_index) + ".png")
+    """
+    sharper_frames_along_most_significant_edges(directory_for_results="enhanced_images",
+                                                folder_of_input_frames=dir_for_denoised_frames,
+                                                image_prefix="denoised_frame_",
+                                                threshold_of_significance=0.9,
+                                                min_value_of_significant_edge=200)
 
     interpolated_images = interpolate_images(img.open("Chessboard_0.png"), img.open("Chessboard_1.png"), 10)
     for img_index, image in enumerate(interpolated_images):
@@ -283,7 +369,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    put_images_side_by_side("Chessboard_images", "Chessboard_", "Chessboard_", 0, "png", "Parallel_images")
-    #a = convert_rgb_image_to_grayscale_image(img.open("lachtan.jpg"))
-    #a.show()
-    #b = make_grayscale_image_sharper(a)
+    main()
